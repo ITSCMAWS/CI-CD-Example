e24644e0e5802911db379de0adaaf0bed01c4620
ITSCMAWS <ITSCMAWS@uww.edu>
2025-09-17 21:20:33 -0500

    Fix 14

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 # main.py
-import os
+
 from flask import Flask
-	
+
 app = Flask(__name__)
-		
+
 @app.route('/')
 def hello_world():
-# A simple greeting
     return "Hello, DevOps Students! Version 2.0"
-		
-if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))

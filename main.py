3a72b055a4afb87a6aeebf90e8b29a515aa946cf
ITSCMAWS <160443728+ITSCMAWS@users.noreply.github.com>
2025-09-18 14:51:32 -0500

    Add main block to run app with debug mode

--- main.py
+++ main.py
@@ -7,3 +7,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return "Hello, DevOps Students! Version 3.0"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))

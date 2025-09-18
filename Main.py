35bd0692472f356a7c3002e68c6b844c5e5656a5
ITSCMAWS <ITSCMAWS@uww.edu>
2025-09-17 20:12:15 -0500

    FIrst change

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 # A simple greeting
-    return "Hello, DevOps Students! Version 1.0"
+    return "Hello, DevOps Students! Version 2.0"
 		
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))

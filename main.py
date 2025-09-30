8186b27a2edf2d69477d3cae6c3d52d49dfd5a5e
ITSCMAWS <ITSCMAWS@uww.edu>
2025-09-30 15:05:25 -0500

    New packers

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello, DevOps Students! Version 666.0"
+    return "GoPackers"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
